d7b71d280e6db01930000e879ee9ec78cb7dac06
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-04-20 03:55:01 -0400

    created postList component
    created PostList component to mount the fetchposts in componentDIdMount.
    added thunk to our index.js. created jsonplaceholder to export axios with the base url of our jsonplaceholder api. added stylesheet semantic ui. created an action to get the posts from our api

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App =()=>{
-    return <div className="ui container">App</div>
+    return (
+        <div>
+            <PostList/>
+        </div>
+    );
 }
 
 export default App;
